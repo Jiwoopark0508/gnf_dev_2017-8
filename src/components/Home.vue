0471998eb2bcef7b3d9ae9b4b45403f76f3b083d
jiwoo park <jiwoop.jj@gmail.com>
2017-12-20 10:58:45 +0900

    Responsive Table, and modify text

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -14,6 +14,7 @@
                 <thead>
                     <tr >
                         <th v-for="head in headers" :key="head.id"
+                            :class='{"hidden-sm-and-down" : head.isHidden}'
                         >
                             {{head.text}}
                         </th>
@@ -29,8 +30,8 @@
                         <td>{{item.time}}</td>
                         <td>{{item.program}}</td>
                         <td>{{item.speaker}}</td>
-                        <td>{{item.location}}</td>
-                        <td>{{item.etc}}</td>
+                        <td class="hidden-sm-and-down">{{item.location}}</td>
+                        <td class="hidden-sm-and-down">{{item.etc}}</td>
                     </tr>
                 </tbody>
             </table>
@@ -48,12 +49,12 @@ export default {
         return {
             text: testmd,
             headers: [
-                { text: 'Date', align: 'left', value: 'date', sortable: false, id:1 },
-                { text: 'Time', align: 'right', value: 'time',sortable: false, id:2 },
-                { text: 'Program', align: 'right',value: 'program', sortable: false, id:3 },
-                { text: 'Speaker', align: 'right',value: 'speaker',sortable: false, id:4 },
-                { text: 'Location', align: 'right',value: 'location',sortable: false, id:5 },
-                { text: 'Etc', align: 'right',value: 'etc',sortable: false, id:6 },
+                { text: 'Date', align: 'left', value: 'date', sortable: false, id:1, isHidden:false },
+                { text: 'Time', align: 'right', value: 'time',sortable: false, id:2, isHidden:false },
+                { text: 'Program', align: 'right',value: 'program', sortable: false, id:3, isHidden:false },
+                { text: 'Speaker', align: 'right',value: 'speaker',sortable: false, id:4, isHidden:false },
+                { text: 'Location', align: 'right',value: 'location',sortable: false, id:5, isHidden:true },
+                { text: 'Etc', align: 'right',value: 'etc',sortable: false, id:6, isHidden:true },
             ],
             items: schedule
         }
