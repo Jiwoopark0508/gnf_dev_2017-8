79c702681b1d8b994602157c851545bbcf77afa6
jiwoo park <jiwoop.jj@gmail.com>
2017-12-23 00:40:05 +0900

    Seperate of Concern (Navbar, Footer, Description)

---
+++ frontend/src/components/Description.vue
@@ -0,0 +1,18 @@
+<template>
+    <v-card dark class="pa-1">
+        <v-card-text v-html="text">
+            {{text}}
+        </v-card-text>
+    </v-card>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
+
